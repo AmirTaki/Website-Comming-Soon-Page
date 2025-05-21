daa0bdc7298c0d28724f18b310b8fe3d0b7c4dee
amir <amirtaki623@gmail.com>
2025-05-21 22:08:13 +0330

    js : days & month & min & sec

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ var countDownDate = new Date("Oct 18 2025 00:00:00").getTime()
 var x = setInterval(() => {
     var now = new Date().getTime()
     var distance = countDownDate - now
-    
-    console.log(distance)
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+
+
 }, 1000)
