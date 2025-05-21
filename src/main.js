8360bb44010394f782e42071369cbd62f3e24d98
amir <amirtaki623@gmail.com>
2025-05-21 22:13:41 +0330

    js : inner html

--- src/main.js
+++ src/main.js
@@ -10,7 +10,9 @@ var x = setInterval(() => {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-
-
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("minutes").innerHTML = minutes
+    document.getElementById("seconds").innerHTML = seconds
 
 }, 1000)
